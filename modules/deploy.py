f7cd7aa4c9fcf67a560964c00c4739bd8a92f5a5
Dan <5505389+DHerls@users.noreply.github.com>
2020-07-24 18:00:59 -0400

    Deploy uses cloudformation now

--- modules/deploy.py
+++ modules/deploy.py
@@ -1,58 +1,76 @@
 import argparse
 import logging
-from common.constants import FUNCTIONS, LAYERS, FLASHBULB_BUCKET_PREFIX
+from common.constants import FUNCTIONS, LAYERS, FLASHBULB_DIR, FLASHBULB_BUCKET_PREFIX
 from common.utils import get_function_by_key, get_function_name, get_function_s3_key, get_layer_name, get_layer_s3_key
 import re
 import boto3
+import time
+import asyncio
 
 logger = logging.getLogger('flashbulb.deploy')
 
-def deploy_layer(key, region):
-    aws_lambda = boto3.client('lambda', region_name=region)
-    logger.info("Deploying {} layer in {}".format(key.title(), region))
-    response = aws_lambda.publish_layer_version(
-        LayerName=get_layer_name(key),
-        Description=str(LAYERS[key]['version']),
-        Content={
-            'S3Bucket': FLASHBULB_BUCKET_PREFIX + region,
-            'S3Key': get_layer_s3_key(key)
-        },
-        CompatibleRuntimes=LAYERS[key]['runtimes'],
+async def deploy_region(region, role_arn):
+    aws_cloudformation = boto3.client('cloudformation', region_name=region)
+    if len(aws_cloudformation.describe_stacks(StackName='Flashbulb')['Stacks']) != 0:
+        logger.warning("Flashbulb already deployed in {}, try updating instead.".format(region))
+        return
+
+    logger.info("Deploying Flashbulb in {}".format(region))
+    template_path = FLASHBULB_DIR.joinpath('assets').joinpath('cloudformation_template.json')
+    with open(template_path, 'r') as f:
+        template_body = f.read()
+
+    response = aws_cloudformation.create_stack(
+        StackName='Flashbulb',
+        TemplateBody=template_body,
+        Parameters=[
+            {
+                'ParameterKey': 'ChromiumVersion',
+                'ParameterValue': str(LAYERS['chromium']['version']),
+            },
+            {
+                'ParameterKey': 'WappalyzerVersion',
+                'ParameterValue': str(LAYERS['wappalyzer']['version']),
+            },
+            {
+                'ParameterKey': 'ScreenshotVersion',
+                'ParameterValue': str(FUNCTIONS['screenshot']['version']),
+            },
+            {
+                'ParameterKey': 'AnalyzeVersion',
+                'ParameterValue': str(FUNCTIONS['analyze']['version']),
+            },
+            {
+                'ParameterKey': 'LambdaRoleArn',
+                'ParameterValue': role_arn,
+            },
+        ],
+        ResourceTypes=[
+            'AWS::Lambda::*',
+        ]
     )
-    return response
+
+    stack_status = 'CREATE_IN_PROGRESS'
+    while stack_status == 'CREATE_IN_PROGRESS':
+        await asyncio.sleep(5)
+        response = aws_cloudformation.describe_stacks(StackName='Flashbulb')
+        stack_status = response['Stacks'][0]['StackStatus']
+    
+    if stack_status == 'CREATE_COMPLETE':
+        logger.info("Successfully deployed Flashbulb in {}".format(region))
+    else:
+        logger.error("Error deploying Flashbulb in {}, status {}".format(region, stack_status))
 
 
-def deploy_function(key, region, role_arn):
-    layer_arns = []
-    for layer in FUNCTIONS[key]['layers']:
-        layer_arns.append(deploy_layer(layer, region)['LayerVersionArn'])
-    logger.info("Deploying {} function in {}".format(key.title(), region))
-    aws_lambda = boto3.client('lambda', region_name=region)
-    aws_lambda.create_function(
-        FunctionName=get_function_name(key),
-        Runtime=FUNCTIONS[key]['runtime'],
-        Role=role_arn,
-        Handler=FUNCTIONS[key]['handler'],
-        Code={
-            'S3Bucket': FLASHBULB_BUCKET_PREFIX + region,
-            'S3Key': get_function_s3_key(key)
-        },
-        Description=str(FUNCTIONS[key]['version']),
-        Timeout=FUNCTIONS[key]['timeout'],
-        MemorySize=FUNCTIONS[key]['memory'],
-        Publish=True,
-        Layers=layer_arns
-    )
-
-
-def deploy_region(region, role_arn):
-    for key, function in FUNCTIONS.items():
-        result = get_function_by_key(key, region)
-        if result is None:
-            deploy_function(key, region, role_arn)
+async def deploy_async(options):
+    tasks = []
+    for region in options.regions:
+        tasks.append(asyncio.create_task(deploy_region(region, options.role_arn)))
+    for task in tasks:
+        await task
+    logger.info('Done deploying Flashbulb across {} region{}'.format(
+        len(options.regions), 's' if len(options.regions) > 1 else ''))
 
 
 def deploy_regions(options):
-    for region in options.regions:
-        deploy_region(region, options.role_arn)
-    logger.info('Done deploying Flashbulb across {} region{}'.format(len(options.regions), 's' if len(options.regions) > 1 else ''))
+    asyncio.run(deploy_async(options))
